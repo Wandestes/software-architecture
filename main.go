fdc9f4ffbe8d2bab2f6694cea303effa4dca09b0
gothicenemy <nazar.maksimovskiy@gmail.com>
2025-02-24 11:54:09 +0200

    [Commit 4] Editing main.go Making local server which shows current time

--- main.go
+++ main.go
@@ -1,1 +1,24 @@
-package mainimport (	"fmt"	"time")func main() {	fmt.Println("What time is it now?🤔")	fmt.Println("The time is", time.Now())}
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+	"time"
+)
+
+type TimeResponse struct {
+	Time string `json:"time"`
+}
+
+func timeHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	response := TimeResponse{Time: time.Now().Format(time.RFC3339)}
+	json.NewEncoder(w).Encode(response)
+}
+
+func main() {
+	http.HandleFunc("/time", timeHandler)
+	port := ":8795"
+	println("Server is running on http://localhost" + port)
+	http.ListenAndServe(port, nil)
+}
