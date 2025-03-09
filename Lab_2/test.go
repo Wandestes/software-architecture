227da1e6678c822b8f958f99a87eb8e245da5e8c
Wandestes <gilua5568@gmail.com>
2025-03-09 12:00:24 +0200

    Add  tests
    Create test.go

---
+++ Lab_2/test.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+    "fmt"
+    "testing"
+    "github.com/stretchr/testify/assert"
+)
+
+func TestConvertPrefixToLisp(t testing.T) {
+    cases := []struct {
+        input    string
+        expected string
+        hasError bool
+    }{
+        {"+ 5 - 4 2 ^ 3 2", "(+ 5 (* (- 4 2) (pow 3 2)))", false},
+        {"- 10 2", "(- 10 2)", false},
+        {"* + 1 2 3", "(* (+ 1 2) 3)", false},
+        {"", "", true},
+        {"invalid input", "", true},
+    }
+
+    for , tc := range cases {
+        result, err := ConvertPrefixToLisp(tc.input)
+        if tc.hasError {
+            assert.Error(t, err)
+        } else {
+            assert.NoError(t, err)
+            assert.Equal(t, tc.expected, result)
+        }
+    }
+}
+
+func ExampleConvertPrefixToLisp() {
+    result,  := ConvertPrefixToLisp("+ 5 * - 4 2 ^ 3 2")
+    fmt.Println(result)
+    // Output: (+ 5 (* (- 4 2) (pow 3 2)))
+}
