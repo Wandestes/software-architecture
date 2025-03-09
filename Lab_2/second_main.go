328b04b2d766e5ca3330520cf210a79999e306c0
gothicenemy <154024313+gothicenemy@users.noreply.github.com>
2025-03-09 12:54:05 +0200

    Update second_main.go

--- Lab_2/second_main.go
+++ Lab_2/second_main.go
@@ -1,55 +1,15 @@
 package main
 
 import (
-    "errors"
-    "strings"
+	"fmt"
+	"log"
 )
 
-// ConvertPrefixToLisp converts a prefix expression to a Lisp-like expression
-func ConvertPrefixToLisp(expression string) (string, error) {
-    if expression == "" {
-        return "", errors.New("empty expression")
-    }
-    tokens := strings.Fields(expression)
-    index := 0
-    result, err := parsePrefix(tokens, &index)
-    if err != nil {
-        return "", err
-    }
-    return result, nil
-}
-
-// parsePrefix recursively parses a prefix expression
-func parsePrefix(tokens []string, index *int) (string, error) {
-    if *index >= len(tokens) {
-        return "", errors.New("invalid expression")
-    }
-    token := tokens[*index]
-    *index++
-
-    if isOperator(token) {
-        left, err := parsePrefix(tokens, index)
-        if err != nil {
-            return "", err
-        }
-        right, err := parsePrefix(tokens, index)
-        if err != nil {
-            return "", err
-        }
-        if token == "^" {
-            token = "pow"
-        }
-        return "(" + token + " " + left + " " + right + ")", nil
-    }
-
-    return token, nil
-}
-
-// isOperator checks if a token is a valid operator
-func isOperator(token string) bool {
-    switch token {
-    case "+", "-", "*", "/", "^":
-        return true
-    }
-    return false
+func main() {
+	expression := "+ 1 * 2 3"
+	result, err := ConvertPrefixToLisp(expression)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(result)
 }
